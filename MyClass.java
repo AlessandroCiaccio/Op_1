4dad82a875c139b183f1228549b383a0df63da5b
Alessandro <sandroscro@libero.it>
2023-10-11 15:30:14 +0200

    Added Main

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=12;
+      int y=3;
+      int q=x/y;
+
+      System.out.println("The quotient of x/y is " + q);
+    }
+}
