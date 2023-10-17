43c95a78648f8489cfbf915c3b2ea2262e81f538
Alessandro <sandroscro@libero.it>
2023-10-17 11:40:47 +0200

    Correzione Main

--- MyClass.java
+++ MyClass.java
@@ -2,8 +2,10 @@ public class MyClass {
     public static void main(String args[]) {
       int x=12;
       int y=3;
-      int q=x/y;
 
-      System.out.println("The quotient of x/y is " + q);
+      System.out.println("The quotient of "+x+"/"+y+" is " + quoziente(x,y));
+    }
+    public static int quoziente(int x, int y){
+        return x/y;
     }
 }
